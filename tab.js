ec2501b19014f7d758a3a0461e9a85a8fbaa4961
SeungheeSong <dgh07027@gmail.com>
2022-05-03 15:01:25 +0900

    탭기능 for2

--- tab.js
+++ tab.js
@@ -1,8 +1,7 @@
 let tabBtn = $('.tab-button');
 let tabCont = $('.tab-content');
-// let className = 
 
-for(let i = 0; i<3; i++){
+for(let i = 0; i<tabBtn.length; i++){
     tabBtn.eq(i).on('click', function(){
         tabBtn.removeClass('orange');
         tabBtn.eq(i).addClass('orange');
@@ -10,24 +9,4 @@ for(let i = 0; i<3; i++){
         tabCont.eq(i).addClass('show');
     }); 
 }
-
-tabBtn.eq(0).on('click', function(){
-    tabBtn.removeClass('orange');
-    tabBtn.eq(0).addClass('orange');
-    tabCont.removeClass('show');
-    tabCont.eq(0).addClass('show');
-});
-
-// tabBtn.eq(1).on('click', function(){
-//     tabBtn.removeClass('orange');
-//     tabBtn.eq(1).addClass('orange');
-//     tabCont.removeClass('show');
-//     tabCont.eq(1).addClass('show');
-// });
-// tabBtn.eq(2).on('click', function(){
-//     tabBtn.removeClass('orange');
-//     tabBtn.eq(2).addClass('orange');
-//     tabCont.removeClass('show');
-//     tabCont.eq(2).addClass('show');
-// });
 
